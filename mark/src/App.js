350872d13337e23aced9425b989e593d03118f1f
cookiemuffins <96406902+cookiemuffins@users.noreply.github.com>
2024-04-06 15:53:25 -0700

    im scared so many files

--- mark/src/App.js
+++ mark/src/App.js
@@ -1,18 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import HikeContainer from './HikeContainer';
+// import { BrowseRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Mark </h1>
-        <HikeContainer
-          imageUrl={"https://www.grousemountain.com/media/W1siZiIsIjIwMjMvMDQvMjAvMTYvMTYvNDkvNGFjNzRiYjEtNjNhOC00NDE0LTgyZDUtNmM5MDFmNTJmMmM5L1RyYW1fTW91bnRhaW5fSW1hZ2UtNy5wbmciXSxbInAiLCJ0aHVtYiIsIjk2MHg0NzBcdTAwM2UiXV0?sha=eb89769b72329847"}
-          name="Grouse"
-          rating="4.5"/>
-      </header>
+    <header className="App-header">
+      <h1> Mark </h1>
+      <HikeContainer
+        imageUrl={"https://www.grousemountain.com/media/W1siZiIsIjIwMjMvMDQvMjAvMTYvMTYvNDkvNGFjNzRiYjEtNjNhOC00NDE0LTgyZDUtNmM5MDFmNTJmMmM5L1RyYW1fTW91bnRhaW5fSW1hZ2UtNy5wbmciXSxbInAiLCJ0aHVtYiIsIjk2MHg0NzBcdTAwM2UiXV0?sha=eb89769b72329847"}
+        name="Grouse Mountain"
+        rating="4.5"
+        url = "https://www.grousemountain.com/"/>
+
+      <HikeContainer
+        imageUrl={"https://happiestoutdoors.ca/wp-content/uploads/2023/07/MiddleJoffreLakeView.jpg"}
+        name="Joffre Lakes"
+        rating="4.8"
+        url = "https://bcparks.ca/joffre-lakes-park/"/>
+    </header>
     </div>
+
   );
 }
 
--- mark/src/HikeContainer.js
+++ mark/src/HikeContainer.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
 import './HikeContainer.css';
+import { Link } from 'react-router-dom';
 
-function HikeContainer({ imageUrl, name, rating}) {
+
+function HikeContainer({ imageUrl, name, rating, url}) {
+    // let history = useHistory();
 
     return (
         <div className="hike-container">
             <img className="hike-img" src={imageUrl} alt={'a hiking destination'} />
             <h3> {name} </h3>
             <p> {rating} </p>
-            <button onClick="doSmth()">See More</button>
+            <button 
+                role="link" 
+                onClick={() => window.open(url)}>See More</button>
         </div>
     )
 }
+
+
 
 
 

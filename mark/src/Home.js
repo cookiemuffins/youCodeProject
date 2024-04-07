bf0783f84c9c99afb10aa9a64291616aee48730f
miamakino <mia.makino@gmail.com>
2024-04-07 11:14:44 -0700

    ADD: more images to homepage

--- mark/src/Home.js
+++ mark/src/Home.js
@@ -1,13 +1,31 @@
 import './Home.css';
 import logo from './assets/homepage.jpg';
+import image1 from './assets/homepagebear1.png';
+import image2 from './assets/homepagebear2.png';
+import image3 from './assets/homepagebear3.png';
 
 function Home() {
   return (
     <div className="container">
-      <div className="image-container"></div>
-      <div className="overlay-text">Mark Your Adventure.</div>
+      <div className="image-container">
+        <div className="overlay-text">Mark Your Adventure.</div>
+      </div>
+
+      <h3>I'd like to see...</h3>
+
+      <div className="image-row">
+        <img src={image1} alt="Image 1" className="image" />
+        <img src={image2} alt="Image 2" className="image" />
+        <img src={image3} alt="Image 3" className="image" />
+      </div>
+
     </div>
+
+    
+
   );
 }
 
 export default Home;
+
+
